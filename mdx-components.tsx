8b4c12d92ee92c7d8e965dcb0b53b9dc7962f470
Zafree <zafree@Zafrees-MacBook-Pro.local>
2024-05-02 01:42:29 +0600

    mdx button

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,11 +1,13 @@
 import type { MDXComponents } from 'mdx/types'
 import s from './mdx-components.module.sass'
 import Separator from '@/components/separator/Separator'
+import Button from '@/components/button/Button'
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
     h1: ({ children }) => <h3 className={s.title}>{children}</h3>,
     h2: ({ children }) => <h4 className={s.subtitle}>{children}</h4>,
+    a: ({ href, children }) => <Button to={href}>{children}</Button>,
     p: ({ children }) => <p className={s.copy}>{children}</p>,
     ul: ({ children }) => <ul className={s.copy}>{children}</ul>,
     ol: ({ children }) => <ol className={s.copy}>{children}</ol>,
