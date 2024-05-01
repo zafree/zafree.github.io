3ce83064280ad57bc926c12d00d37bad43c71713
Zafree <zafree@Zafrees-MacBook-Pro.local>
2024-05-02 03:40:45 +0600

    image unoptimaze

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,9 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: '',
-  // output: 'export',
+  output: 'export',
   pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  images: { unoptimized: true },
 }
 
 const withMDX = createMDX({
