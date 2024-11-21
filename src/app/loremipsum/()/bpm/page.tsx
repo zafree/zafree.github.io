d3e96a1a0567fbce296fa03c5a49ca221568f1d5
Zafree <zafree@Zafrees-MacBook-Pro.local>
2024-11-22 02:25:10 +0600

    loremipsum

--- src/app/loremipsum/()/bpm/page.tsx
+++ src/app/loremipsum/()/bpm/page.tsx
@@ -2,9 +2,8 @@
 
 import React from 'react'
 import Hero from '@/components/hero/Hero'
-import { getWorksSlug } from '@/api/works/getWorks'
+import { getPostsSlug } from '@/api/posts/getPosts'
 import { usePathname } from 'next/navigation'
-import Post from './post.mdx'
 import Content, { Row } from '@/components/content/Content'
 
 import Intro from './intro.mdx'
@@ -12,28 +11,28 @@ import WebDesign from './web-design.mdx'
 import BUBTAwesome from './bubt-awesome.mdx'
 import HeroImage from '@/components/hero-image/HeroImage'
 import Pilpil from '@/components/pilpil/Pilpil'
-import BrowseMore from '@/components/works/BrowseMore'
+import ReadMore from '@/components/posts/ReadMore'
 
 function BPM() {
   const url = usePathname()
   const slug = url.substring(url.lastIndexOf('/') + 1)
-  const work = getWorksSlug(slug)
+  const post = getPostsSlug(slug)
   return (
     <>
-      <Hero title={work?.title as string} subtitle={work?.subtitle} />
+      <Hero title={post?.title as string} subtitle={post?.subtitle} />
       <HeroImage>
         <Pilpil
-          width={1920}
-          height={960}
-          small={`/work/${work?.slug}/cover-small.jpg`}
-          large={`/work/${work?.slug}/cover.jpg`}
-          alt={`${work?.title} cover image`}
+          width={1280}
+          height={640}
+          small={`${url}/cover-small.jpg`}
+          large={`${url}/cover.jpg`}
+          alt={`${post?.title} cover image`}
         />
       </HeroImage>
       <Intro />
       <WebDesign />
       <BUBTAwesome />
-      <BrowseMore />
+      <ReadMore />
     </>
   )
 }
--- src/app/loremipsum/()/pmi/page.tsx
+++ src/app/loremipsum/()/pmi/page.tsx
@@ -2,33 +2,33 @@
 
 import React from 'react'
 import Hero from '@/components/hero/Hero'
-import { getWorksSlug } from '@/api/works/getWorks'
+import { getPostsSlug } from '@/api/posts/getPosts'
 import { usePathname } from 'next/navigation'
 import HeroImage from '@/components/hero-image/HeroImage'
 import Pilpil from '@/components/pilpil/Pilpil'
 import PmiBangladesh from './PmiBangladesh.mdx'
 import Endorsement from '@/components/endorsements/Endorsement'
-import BrowseMore from '@/components/works/BrowseMore'
+import ReadMore from '@/components/posts/ReadMore'
 
 function PagePilpil() {
   const url = usePathname()
   const slug = url.substring(url.lastIndexOf('/') + 1)
-  const work = getWorksSlug(slug)
+  const post = getPostsSlug(slug)
   return (
     <>
-      <Hero title={work?.title as string} subtitle={work?.subtitle} />
+      <Hero title={post?.title as string} subtitle={post?.subtitle} />
       <HeroImage>
         <Pilpil
           width={1280}
           height={640}
-          small={`/work/${work?.slug}/small-thumb.jpg`}
-          large={`/work/${work?.slug}/cover.jpg`}
-          alt={`${work?.title} cover image`}
+          small={`/loremipsum/${post?.slug}/small-thumb.jpg`}
+          large={`/loremipsum/${post?.slug}/cover.jpg`}
+          alt={`${post?.title} cover image`}
         />
       </HeroImage>
       <PmiBangladesh />
-      <Endorsement load="shafiq" />
-      <BrowseMore />
+      {/* <Endorsement load="shafiq" /> */}
+      <ReadMore />
     </>
   )
 }
