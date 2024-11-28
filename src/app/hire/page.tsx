e9d1110231c53e8ccb75108f9225c32d89528fab
Zafree <zafree@Zafrees-MacBook-Pro.local>
2024-11-28 20:38:45 +0600

    update hire

--- src/app/hire/page.tsx
+++ src/app/hire/page.tsx
@@ -14,25 +14,18 @@ export const metadata: Metadata = {
 function Hire() {
   return (
     <>
-      <Hero kicker="Hire me" title="I’m a design-minded front-end developer.">
+      <Hero kicker="Hire me" title="I’m a dev-minded designer.">
         <h2 className={s.Hire__subheading}>
           Also called{' '}
           <Button to="https://bradfrost.com/blog/post/frontend-design/">
-            front-end designer
+            Front-End Designer
           </Button>
-          , UI developers, UI engineer, or{' '}
+          , UX Engineer, or{' '}
           <Button to="https://snook.ca/archives/opinion/design-engineering">
-            design engineer
+            Design Engineer
           </Button>
           .
         </h2>
-        <p className={s.Hire__copy}>
-          As a <em>[choose one of the above titles]</em>, I work on the{' '}
-          <Button to="https://bradfrost.com/blog/post/frontend-design/">
-            front-end of the front-end
-          </Button>
-          . That is, my work lies at the intersection of design and back-end.
-        </p>
       </Hero>
       <Tldr />
     </>
